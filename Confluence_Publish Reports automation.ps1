8d3d3b9573d157c17704d48f61de48f41e6fb58b
kakdesanket <kakdesanket@gmail.com>
2020-08-20 15:22:38 +0530

    Update Confluence_Publish Reports automation.ps1

--- Confluence_Publish Reports automation.ps1
+++ Confluence_Publish Reports automation.ps1
@@ -4,17 +4,17 @@ Import-Module PSSQLite
 ###Confluence Page Vulnerabilities by Severity###
 Write-Host "###Confluence Page Vulnerabilities by Severity###" -ForegroundColor Red
 Write-Host "Connecting to Confluence , please input credentials" -ForegroundColor Green
-Set-ConfluenceInfo -BaseURI 'https://confluence.elkjop.com' -PromptCredentials
-$Page1=Get-ConfluencePage -PageID 33895711
+Set-ConfluenceInfo -BaseURI 'https://confluence.client.com' -PromptCredentials
+$Page1=Get-ConfluencePage -PageID 11111111
 Write-Host "Fetching Information from Confluence for page" $Page1 -ForegroundColor Green
 Write-Host "Current Data on Page :"
 $Page1.Body
-$Database="C:\Users\sanket\Desktop\Qualys.db"
+$Database="C:\Users\Desktop\Qualys.db"
 Write-Host "Fetching Information from Database" -ForegroundColor Green
 $Query1="SELECT Severity, COUNT(DISTINCT Title) AS Count FROM VulnerabilityDatabase GROUP BY Severity ORDER BY Severity"
 $InPDatabase1=Invoke-SqliteQuery -Query $Query1 -DataSource $Database
 $NormalizeData1=$InPDatabase1|ConvertTo-ConfluenceTable |ConvertTo-ConfluenceStorageFormat
-Set-ConfluencePage -PageID 33895711 -Body $NormalizeData1
+Set-ConfluencePage -PageID 11111111 -Body $NormalizeData1
 Write-Host "Updated Data on Page :"
 $Page1.Body
 Write-Host "#####End Confluence Page Vulnerabilities by Severity###" -ForegroundColor Red
@@ -23,7 +23,7 @@ Write-Host "#####End Confluence Page Vulnerabilities by Severity###" -Foreground
 
 ###Confluence Page Top 10 Vulnerabilities of the day ###
 Write-Host "###Confluence Page Top 10 Vulnerabilities of the day ###" -ForegroundColor Red
-$Page2=Get-ConfluencePage -PageID 33895589
+$Page2=Get-ConfluencePage -PageID 11111111
 Write-Host "Fetching Information from Confluence for page" $Page2 -ForegroundColor Green
 Write-Host "Current Data on Page :"
 $Page2.Body
@@ -31,7 +31,7 @@ Write-Host "Connecting to Confluence " -ForegroundColor Red
 $Query2="SELECT Title AS Vulnerablity, COUNT(Severity) AS Occurence FROM VUlnerabilityDatabase WHERE Severity > 3 GROUP BY Title ORDER BY Occurence DESC LIMIT 10"
 $InPDatabase2=Invoke-SqliteQuery -Query $Query2 -DataSource $Database
 $NormalizeData2=$InPDatabase2|ConvertTo-ConfluenceTable |ConvertTo-ConfluenceStorageFormat
-Set-ConfluencePage -PageID 33895589 -Body $NormalizeData2
+Set-ConfluencePage -PageID 11111111 -Body $NormalizeData2
 Write-Host "Updated Data on Page :"
 $Page2.Body
 Write-Host "####End Confluence Page Top 10 Vulnerabilities of the day ###" -ForegroundColor Red
@@ -40,7 +40,7 @@ Write-Host "####End Confluence Page Top 10 Vulnerabilities of the day ###" -Fore
 
 ###Confluence Page Distinct Vulnerability Database ###
 Write-Host "###Confluence Page Distinct Vulnerability Database ###" -ForegroundColor Red
-$Page3=Get-ConfluencePage -PageID 33894753
+$Page3=Get-ConfluencePage -PageID 11111111
 Write-Host "Fetching Information from Confluence for page" $Page3 -ForegroundColor Green
 Write-Host "Current Data on Page :"
 $Page3.Body
@@ -49,7 +49,7 @@ Write-Host "Connecting to Confluence " -ForegroundColor Red
 $Query3="SELECT DISTINCT IP, Title, Severity FROM VulnerabilityDatabase ORDER by Severity DESC "
 $InPDatabase3=Invoke-SqliteQuery -Query $Query3 -DataSource $Database
 $NormalizeData3=$InPDatabase3|ConvertTo-ConfluenceTable |ConvertTo-ConfluenceStorageFormat
-Set-ConfluencePage -PageID 33894753 -Body $NormalizeData3
+Set-ConfluencePage -PageID 11111111 -Body $NormalizeData3
 Write-Host "Updated Data on Page :"
 $Page3.Body
 Write-Host "####End Confluence Page Distinct Vulnerability Database ###" -ForegroundColor Red
@@ -58,7 +58,7 @@ Write-Host "####End Confluence Page Distinct Vulnerability Database ###" -Foregr
 
 ###Confluence Page Deep Security Database ###
 Write-Host "###Confluence Page Deep Security Database ###" -ForegroundColor Red
-$Page4=Get-ConfluencePage -PageID 33898285
+$Page4=Get-ConfluencePage -PageID 11111111
 Write-Host "Fetching Information from Confluence for page" $Page3 -ForegroundColor Green
 Write-Host "Current Data on Page :"
 $Page4.Body
@@ -66,7 +66,7 @@ Write-Host "Connecting to Confluence " -ForegroundColor Red
 $Query4="SELECT DISTINCT * FROM DeepSecurity "
 $InPDatabase4=Invoke-SqliteQuery -Query $Query4 -DataSource $Database
 $NormalizeData4=$InPDatabase4|ConvertTo-ConfluenceTable |ConvertTo-ConfluenceStorageFormat
-Set-ConfluencePage -PageID 33898285 -Body $NormalizeData4
+Set-ConfluencePage -PageID 11111111 -Body $NormalizeData4
 Write-Host "Updated Data on Page :"
 $Page4.Body
 Write-Host "####End Confluence Page Distinct Vulnerability Database ###" -ForegroundColor Red
